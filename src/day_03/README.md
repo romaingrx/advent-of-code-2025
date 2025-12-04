0a7d7acaf8e6e19a48f224a972287ddb276d6818
Romain Graux <48758915+romaingrx@users.noreply.github.com>
2025-12-04 11:37:27 +0100

    feat(day03): working part 1

--- src/day_03/README.md
+++ src/day_03/README.md
@@ -14,6 +14,7 @@ There are batteries nearby that can supply emergency power to the escalator for 
 811111111111119
 234234234234278
 818181911112111
+
 The batteries are arranged into banks; each line of digits in your input corresponds to a single bank of batteries. Within each bank, you need to turn on exactly two batteries; the joltage that the bank produces is equal to the number formed by the digits on the batteries you've turned on. For example, if you have a bank like 12345 and you turn on batteries 2 and 4, the bank would produce 24 jolts. (You cannot rearrange batteries.)
 
 You'll need to find the largest possible joltage each bank can produce. In the above example:
--- src/day_03/mod.rs
+++ src/day_03/mod.rs
@@ -1,4 +1,4 @@
-use std::fs;
+use std::{cmp::Ordering, fs};
 
 pub fn run(part: u8, is_test: bool) {
     let input_file = if is_test {
@@ -10,10 +10,10 @@ pub fn run(part: u8, is_test: bool) {
         .expect("Failed to read input file");
 
     // Parse input here
-    // let lines: Vec<&str> = input.lines().collect();
+    let banks: Vec<&str> = input.lines().collect();
 
     let result = match part {
-        1 => part1(&input),
+        1 => part1(&banks),
         _ => {
             println!("Part {} not implemented for day 3", part);
             return;
@@ -23,9 +23,30 @@ pub fn run(part: u8, is_test: bool) {
     println!("Day 3 Part {}: {}", part, result);
 }
 
-fn part1(_input: &str) -> i32 {
-    // Implement your solution here
-    0
+fn part1(banks: &[&str]) -> u64 {
+    banks
+        .iter()
+        .enumerate()
+        .map(|(bank_idx, bank)| {
+            let (first_max_idx, first_max_char) = bank
+                .chars()
+                .take(bank.chars().count() - 1)
+                .enumerate()
+                .max_by(|(_, a), (_, b)| a.cmp(b).then(Ordering::Greater))
+                .unwrap();
+
+            let (_, second_max_char) = bank
+                .chars()
+                .skip(first_max_idx + 1)
+                .enumerate()
+                .max_by(|(_, a), (_, b)| a.cmp(b).then(Ordering::Greater))
+                .unwrap();
+            println!("[bank {bank_idx}]: {first_max_char}{second_max_char}");
+            format!("{}{}", first_max_char, second_max_char)
+                .parse::<u64>()
+                .unwrap()
+        })
+        .sum()
 }
 
 #[cfg(test)]
@@ -34,7 +55,18 @@ mod tests {
 
     #[test]
     fn test_example() {
-        let input = "";
-        assert_eq!(part1(input), 0);
+        let input = vec![
+            "987654321111111",
+            "811111111111119",
+            "234234234234278",
+            "818181911112111",
+        ];
+        assert_eq!(part1(&input), 357);
+    }
+
+    #[test]
+    fn test_tricky() {
+        let input = vec!["91111111191"];
+        assert_eq!(part1(&input), 99);
     }
 }
